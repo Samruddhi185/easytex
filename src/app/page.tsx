19fcf31673add067868c19815f7db79924a87173
Bhavesh Vasnani <bhaveshvasnani197@gmail.com>
2023-10-21 00:12:52 -0400

    made things fancy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,22 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className="title">EasyTex</h1>
-      <h3 className="subtitle">Text to LaTeX conversion</h3>
+      <section className="wrapper">
+        <div className="top">EasyTex</div>
+        <div className="bottom" aria-hidden="true">EasyTex</div>
+      </section>
+      <h3 className="typewriter">Text to LaTeX conversion</h3>
       <div className="container">
         <div id="div1">
-          <p>Enter Your Input</p><br></br>
+          <h5>Enter Your Input</h5><br></br>
           <textarea name='textInput' placeholder='Enter text to convert...' rows={17} cols={40}></textarea>
           <button>Generate LaTeX</button>
         </div>
         <div id="div2">
-          <p>LaTeX</p>
+          <h5>LaTeX</h5>
         </div>
         <div id="div3">
-          <p>PDF Preview</p>
+          <h5>PDF Preview</h5>
         </div>
       </div>
     </main>
