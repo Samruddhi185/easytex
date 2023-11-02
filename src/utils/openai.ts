28150ebf03e94b958a3427be0d500eb1b2df5331
Samruddhi Patil <sampatil@cs.stonybrook.edu>
2023-11-01 23:17:10 -0400

    key removed

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,7 +1,7 @@
 import { OpenAI } from 'openai';
 const openai = new OpenAI(
     {
-        apiKey: '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',
+        apiKey: '',
         // dangerouslyAllowBrowser: true
     }
 );
